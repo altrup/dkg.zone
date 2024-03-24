a2e6e29601ad455d5c6d0e5822de4e5757cba0f0
Eric Liu <liueric390@gmail.com>
2024-03-24 15:23:21 -0400

    Set up expressjs with simple home page

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// make all files in public available
+app.use(express.static(path.join(__dirname, 'public')));
+
+// home page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "src/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
